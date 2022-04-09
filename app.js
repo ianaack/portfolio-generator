caab5c7da1c9c71046797744767d015bea48d6e5
ianaack <ianaack@gmail.com>
2022-04-09 14:51:18 -0400

    Feature: Generate Webpage

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
+// global variables
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
 
-const printProfileData = (profileDataArr) => {
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+/* "file name that will be created, or output", dataThats(being,written), (callback function to handle errors/successes)*/
+fs.writeFile("index.html", generatePage(name, github), (err) => {
+  if (err) throw err;
 
-  console.log("+++++++++++++++++++");
-
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
