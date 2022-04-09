d31559ad297083da68e6fa56b17d3be2d2d828d4
ianaack <ianaack@gmail.com>
2022-04-09 15:29:50 -0400

    Feature: Installed inquirer npm

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-// global variables
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+const inquirer = require("inquirer");
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
+// // global variables
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
+// const pageHTML = generatePate(name, github);
 
-/* "file name that will be created, or output", dataThats(being,written), (callback function to handle errors/successes)*/
-fs.writeFile("index.html", generatePage(name, github), (err) => {
-  if (err) throw err;
+// /* "file name that will be created, or output", dataThats(being,written), (callback function to handle errors/successes)*/
+// fs.writeFile("index.html", pageHTML, (err) => {
+//   if (err) throw err;
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
